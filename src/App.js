b7af0dc3d4a0b5141e2f587fd59a556ded0c2458
Hayden Bruin <bruinhayden@gmail.com>
2019-03-04 23:10:16 +1300

    Course #11 useMemo for expensive functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { useRef, useEffect, createContext } from 'react'
+import React, { useRef, useEffect, createContext, useMemo } from 'react'
 import { useTitleInput } from './hooks/use-title-input'
 import Toggle from './toggle'
+import Counter from './counter'
 
 export const UserContext = createContext();
 
 const App = () => {
     const [name, setName] = useTitleInput( '' );
     const ref = useRef();
+
+    const reverseWord = word => {
+        console.log('function called');
+        return word.split('').reverse().join('');
+    }
+
+    const title = 'Level up dishes';
+    const titleReversed = useMemo(() => reverseWord(title), [title])
 
     useEffect( () => {
         console.log( 'ref: ', ref.current.className );
@@ -17,7 +26,7 @@ const App = () => {
             user: false
         }}>
             <div className="main-wrapper" ref={ref}>
-                <h1 onClick={() => console.log( ref.current.className )}>Level Up Dishes</h1>
+                <h1 onClick={() => console.log( ref.current.className )}>{titleReversed}</h1>
 
                 <form onSubmit={e => {
                     e.preventDefault();
@@ -28,6 +37,7 @@ const App = () => {
                     <button>Submit</button>
                 </form>
             </div>
+            
             <Toggle />
         </ UserContext.Provider>
     );
