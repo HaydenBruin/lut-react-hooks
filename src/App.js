9d77d8ace68a6e197717f24cc8e491033c3ce55c
Hayden Bruin <bruinhayden@gmail.com>
2019-03-04 20:42:34 +1300

    Course #9 react context hook

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, createContext } from 'react'
 import { useTitleInput } from './hooks/use-title-input'
+import Toggle from './toggle'
+
+export const UserContext = createContext();
 
 const App = () => {
-    const [name, setName] = useTitleInput('');
+    const [name, setName] = useTitleInput( '' );
     const ref = useRef();
 
-    useEffect(() => {
-        console.log('ref: ', ref.current.className);
-    })
+    useEffect( () => {
+        console.log( 'ref: ', ref.current.className );
+    } )
 
     return (
-        <div className="main-wrapper" ref={ref}>
-            <h1 onClick={() => console.log(ref.current.className)}>Level Up Dishes</h1>
+        <UserContext.Provider value={{
+            user: false
+        }}>
+            <div className="main-wrapper" ref={ref}>
+                <h1 onClick={() => console.log( ref.current.className )}>Level Up Dishes</h1>
 
-            <form onSubmit={e => {
-                e.preventDefault();
-                formSubmit(name, setName);
-            }}>
-                <input type="text" onChange={e => setName(e.target.value)} value={name} />
+                <form onSubmit={e => {
+                    e.preventDefault();
+                    formSubmit( name, setName );
+                }}>
+                    <input type="text" onChange={e => setName( e.target.value )} value={name} />
 
-                <button>Submit</button>
-            </form>
-        </div>
+                    <button>Submit</button>
+                </form>
+            </div>
+            <Toggle />
+        </ UserContext.Provider>
     );
 };
 
-const formSubmit = (value, setValue) => {
-    console.log('Email sent to ' + value);
+const formSubmit = ( value, setValue ) => {
+    console.log( 'Email sent to ' + value );
 
-    setValue('');
+    setValue( '' );
 }
 
 
--- src/toggle.js
+++ src/toggle.js
@@ -1,15 +1,24 @@
 
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { UserContext } from './App'
 
 const Refactor = () => {
-    const [isToggled, setToggled] = useState(false);
+    const [isToggled, setToggled] = useState( false );
+
+    const userInfo = useContext( UserContext );
+
+    if ( !isUserLoggedIn( userInfo ) ) return null;
 
     return (
         <div>
-            <button onClick={() => setToggled(!isToggled)}>Toggle</button>
+            <button onClick={() => setToggled( !isToggled )}>Toggle</button>
             {isToggled && <h2>Hello!</h2>}
         </div>
     );
+}
+
+function isUserLoggedIn( userInfo ) {
+    return userInfo.user;
 }
 
 export default Refactor
