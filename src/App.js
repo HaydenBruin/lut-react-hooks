da5f3109b019004172276d788ac63f6f97c07757
Hayden Bruin <bruinhayden@gmail.com>
2019-03-05 22:52:01 +1300

    Course #10-11, UseEffect and fetch api

--- src/App.js
+++ src/App.js
@@ -1,53 +1,67 @@
-import React, { useRef, useEffect, createContext, useMemo } from 'react'
-import { useTitleInput } from './hooks/use-title-input'
-import Toggle from './toggle'
-import Counter from './counter'
+import React, { useState, useRef, useEffect, createContext } from 'react'
 
 export const UserContext = createContext();
 
 const App = () => {
-    const [name, setName] = useTitleInput( '' );
-    const ref = useRef();
+    const [name, setName] = useState('');
+    const ref = useRef(); 
+    const [dishes, setDishes] = useState([]);
 
-    const reverseWord = word => {
-        console.log('function called');
-        return word.split('').reverse().join('');
+    const fetchDishes = async () => {
+        const res = await fetch('https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes');
+        const data = await res.json();
+        setDishes(data);
     }
+    useEffect(() => {
+        fetchDishes();
+    }, []);
 
-    const title = 'Level up dishes';
-    const titleReversed = useMemo(() => reverseWord(title), [title])
-
-    useEffect( () => {
-        console.log( 'ref: ', ref.current.className );
-    } )
+    const addDish = (e) => {
+        e.preventDefault();
+        setName('');
+        setDishes([
+            {
+                name: name,
+                desc: 'An awesome version of ..',
+                ingredients: [
+                    'one',
+                    'two',
+                    'three'
+                ]
+            },
+            ...dishes]
+        )
+    }
 
     return (
         <UserContext.Provider value={{
             user: false
         }}>
             <div className="main-wrapper" ref={ref}>
-                <h1 onClick={() => console.log( ref.current.className )}>{titleReversed}</h1>
+                <h1 onClick={() => console.log( ref.current.className )}>Level up dishes</h1>
 
-                <form onSubmit={e => {
-                    e.preventDefault();
-                    formSubmit( name, setName );
-                }}>
+                <form onSubmit={(e) => addDish(e)}>
                     <input type="text" onChange={e => setName( e.target.value )} value={name} />
 
                     <button>Submit</button>
                 </form>
+
+                {dishes && dishes.map((dish, index) => (    
+                    <article key={index} className="dish-card dish-card--withImage">
+                        <h3>{dish.name}</h3>
+                        <p>{dish.desc}</p>
+                        <div className="ingredients">
+                            {dish.ingredients.map((ingredient, index) => (
+                                <span key={index}>{ingredient}</span>
+                            ))}
+                        </div>
+                    </article>
+                ))}
             </div>
             
-            <Toggle />
         </ UserContext.Provider>
     );
 };
-
-const formSubmit = ( value, setValue ) => {
-    console.log( 'Email sent to ' + value );
-
-    setValue( '' );
-}
 
 
 
--- src/hooks/use-title-input.js
+++ src/hooks/use-title-input.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useDebugValue } from 'react';
 
 export function useTitleInput(initialValue) {
     const [value, setValue] = useState(initialValue);
@@ -6,6 +6,8 @@ export function useTitleInput(initialValue) {
     useEffect(() => {
         document.title = value;
     })
+
+    useDebugValue(value.length > 0 ? 'Full' : 'Empty');
 
     return [value, setValue];
 }
